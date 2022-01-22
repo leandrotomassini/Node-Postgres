83de34141767e363cafd319578c4432c40432aef
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-01-22 17:06:10 -0300

    Prueba

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    host: 'ec2-3-227-15-75.compute-1.amazonaws.com',
-    user: 'ytdsvzzanmptvo',
-    password: '75a2dcc22143fda327c592b47e0cf0e64c4894f78e381ccec57ef10e3b1e10c6',
-    database: 'd1tsp0qr7gensr',
-    port: 5432
-});
+// const pool = new Pool({
+//     host: 'ec2-3-227-15-75.compute-1.amazonaws.com',
+//     user: 'ytdsvzzanmptvo',
+//     password: '75a2dcc22143fda327c592b47e0cf0e64c4894f78e381ccec57ef10e3b1e10c6',
+//     database: 'd1tsp0qr7gensr',
+//     port: 5432
+// });
 
 const getUsers = async(req, res) => {
-    const response = await pool.query('SELECT * FROM users');
-    console.log(response.rows);
+    // const response = await pool.query('SELECT * FROM users');
+    // console.log(response.rows);
     res.send('users');
 }
 
