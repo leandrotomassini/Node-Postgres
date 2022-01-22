5d658118e7060cf8342777151caaaed4b0343732
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-01-22 17:01:16 -0300

    Probando configuracion online2

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'Universidad11201911',
-    database: 'firstapi',
+    host: 'ec2-3-227-15-75.compute-1.amazonaws.com',
+    user: 'ytdsvzzanmptvo',
+    password: '75a2dcc22143fda327c592b47e0cf0e64c4894f78e381ccec57ef10e3b1e10c6',
+    database: 'd1tsp0qr7gensr',
     port: 5432
 });
 
